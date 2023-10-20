fa2917d2c80e57508337ec04861a45703dda8042
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-19 20:14:41 -0700

    new upated ssaddasfaassaft

--- example_app/example_two.py
+++ example_app/example_two.py
@@ -5,7 +5,7 @@ st.title("Macro Indicators ")
 import plotly.express as px
 data_canada = px.data.gapminder().query("country == 'Canada'")
 fig = px.bar(data_canada, x='year', y='pop')
-st.plotly_chart(fig)
+st.plotly_chart(fig, se_container_width=True)
 
 
 
@@ -16,5 +16,6 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 
 with st.container():
+
     fig_1 = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'])])
-    st.plotly_chart(fig_1)
+    st.plotly_chart(fig_1, se_container_width=True)
