01dcb2d6d52919cb50bbb26e07dc1db2774a8c02
jluna1398 <60058404+jluna1398@users.noreply.github.com>
2023-10-19 20:12:20 -0700

    new upated ssaddasdt

--- example_app/example_two.py
+++ example_app/example_two.py
@@ -11,8 +11,10 @@ st.plotly_chart(fig)
 
 import plotly.graph_objects as go
 
+
 import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
 
-fig_1 = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'])])
-st.plotly_chart(fig_1)
+with st.container():
+    fig_1 = go.Figure([go.Scatter(x=df['Date'], y=df['AAPL.High'])])
+    st.plotly_chart(fig_1)
